746412409cb93e078295c09923036d82443ed4fd
jgustavo <jgustavo0711@gmail.com>
2024-10-09 16:53:53 -0300

    inicial commit

---
+++ src/Teste.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+
+function Teste() {
+  return (
+    <>
+    <Botao fontSize="18px" principal>
+      Enviar
+      </Botao>
+    <Botao fontSize="14px" principal={false}>
+      Cancelar
+    </Botao>
+    </>
+  )
+}
+
+export default Teste
