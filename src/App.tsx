5f536e92a507685eedf916c2fa7ce7d87f66972e
jgustavo <jgustavo0711@gmail.com>
2024-10-23 17:54:30 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './temas/light'
+import temaDark from './temas/dark'
 
 function App() {
+  const [usandoTemaDark, setUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setUsandoTemaDark(!usandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={usandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
